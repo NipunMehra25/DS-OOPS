51484d30fd513b5e03864a7b1d5ec06fc58b56be
NipunMehra25 <nipunmehra25dec@gmail.com>
2025-07-17 09:48:57 +0530

    Create ConstantUsage.cpp on Day 2 Week 1

---
+++ Day 2/ConstantUsage.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define PI 3.14
+using namespace std;
+
+int main() {
+    const int a = 10;
+    cout << "Const value: " << a << endl;
+    cout << "Macro value: " << PI;
+    return 0;
+}
